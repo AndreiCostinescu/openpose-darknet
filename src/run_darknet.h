41154f100bad4b09d659effff23cf9ca15f4fe79
Andrei Costinescu <andrei.costinescu@yahoo.com>
2020-11-07 13:37:43 +0100

    Change run_darknet.c to run_darknet.cpp and use cpp features (vector)

--- src/run_darknet.h
+++ src/run_darknet.h
@@ -1,12 +1,15 @@
 #ifndef RUN_DARKNET_H
 #define RUN_DARKNET_H
 
-extern "C" {
+#include <darknet.h>
+#include <images/image.h>
 
-void initNet(char *cfgFile, char *weightFile, int *inW, int *inH, int *outW, int *outH);
+void initNet(char *dataFile, char *cfgFile, char *weightFile, int benchmarkLayers,
+             int *inW = nullptr, int *inH = nullptr, int *outW = nullptr, int *outH = nullptr, network **_net = nullptr,
+             image ***_alphabet = nullptr, char ***_names = nullptr, layer *_lastDetectionLayer = nullptr);
 
 float *runNet(float *inData);
 
-};
+void releaseNet();
 
 #endif // RUN_DARKNET_H
